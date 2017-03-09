34f5878bcaa13d341eab79942c0fa34635f92e86
Sergiotm87 <sergiotm87@gmail.com>
2017-03-09 20:39:51 +0100

    4: resolviendo ancestros con xpath

--- enunciado4.py
+++ enunciado4.py
@@ -6,5 +6,27 @@ from lxml import etree
 doc = etree.parse('batallasjuegotronos.xml')
 raiz=doc.getroot()
 
-#for e in raiz.findall('//attribute::tamaño_ejercito'):
-    #print e
+#rey=raw_input("introduce el nombre de un rey")
+#ej. Robb Stark,Stannis Baratheon
+rey="Robb Stark"
+
+
+#ejercitos=raiz.xpath("//batalla/contendientes")
+#for i in ejercitos:
+    #print i[0].attrib
+    #print i.attrib[]
+
+
+#for attr,value in ejercitos.items():
+      #print attr,value
+
+#raiz.findall("/batalla/contendientes/ejercito_atacante/rey_atacante")
+
+rutarey="//rey_atacante/text()|//rey_defensor/text()"
+reyes=raiz.xpath(rutarey)
+if rey in reyes:
+    ejercitos=("//ejercito_atacante[rey_atacante/text()="+rey+"]/@tamaño_ejercito")
+
+
+
+
