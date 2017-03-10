654de0854c43401d84478ad3146c6cb0ab78a452
Sergiotm87 <sergiotm87@gmail.com>
2017-03-10 13:39:14 +0100

    5 definicion de rutas xpath a buscar

--- enunciado5.py
+++ enunciado5.py
@@ -6,3 +6,15 @@ from lxml import etree
 #
 doc = etree.parse('batallasjuegotronos.xml')
 raiz=doc.getroot()
+
+zona=raw_input("introduce el nombre de una zona")
+#ej. Deepwood Motte,Winterfell
+
+#localizamos el nombre de las zonas, despues tomamos los reyes que hayan participado en esas zonas y tomamos el valor con mayor numero de
+#coincidencias cuyo resultado sea victoria
+
+rutazona="//localizacion/text()"
+
+rutareyes="//rey_atacante/text()[//localizacion/text()='"+zona+"']"
+
+#falta comprobar resultado = victoria
