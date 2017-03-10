e23ef27a6f73c9a4990846b5f60b8ad122bf6685
Sergiotm87 <sergiotm87@gmail.com>
2017-03-10 12:01:36 +0100

    4: obtenidas las cadenas a utilizar, falta localizar por xpath un valor del padre correspondiente

--- enunciado4.py
+++ enunciado4.py
@@ -13,16 +13,21 @@ rey="Robb Stark"
 #he eliminado el caracter 'ñ' del xml porque xpath no aceptaba ese caracter a pesar de que ambos ficheros estan codificados con utf-8
 
 rutarey="//rey_atacante/text()|//rey_defensor/text()"
-ejercito="//ejercito_atacante[rey_atacante/text()='"+rey+"']/@ejercito"
+
+#ejercito="//ejercito_atacante[rey_atacante/text()='"+rey+"']/@ejercito"
+
+ejercito="//ejercito_atacante[rey_atacante/text()='"+rey+"']/@ejercito| //ejercito_defensor[rey_defensor/text()='"+rey+"']/@ejercito"
+
 
 reyes=raiz.xpath(rutarey)
 if rey in reyes:
     ejercitos=raiz.xpath(ejercito)
 
-#print ejercitos
+print ejercitos
 
 #pasamos la lista de strings a una lista de numeros pero al mapear la lista como integer da una excepcion ValueError debido que no puede
 #pasar a numero un valor como ' ' asi que usamos lo siguiente, la lista sin repetir y una estructura con la misma funcion que map(int,list)
+
 
 x = list(set(ejercitos))
 
@@ -36,4 +41,16 @@ for elem in x:
 
 print mayorejercito
 
-# falta modificar la ruta xpath ejercito para que tome tambien al rey en el ejercito defensor
+#ruta ejercito contrincante con el mismo nodo padre:
+
+ejenemigo="//contendientes/ejercito_atacante[@ejercito="+str(mayorejercito)+"][//rey_atacante/text()='"+rey+"']/following-sibling::ejercito_defensor"
+enemigo=raiz.xpath(ejenemigo)
+
+ejercitoenemigo= enemigo[0].get('ejercito')
+
+print ejercitoenemigo
+
+
+#falta mostrar el nombre de dicha batalla
+
+#print "la mayor batalla de",rey,"fue",nombrebatalla,"en la que participaron",mayorejercito+ejercitoenemigo
