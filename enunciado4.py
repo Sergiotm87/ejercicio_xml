4449c4acd1481100e41fd14b6255e317c7f22987
Sergiotm87 <sergiotm87@gmail.com>
2017-03-10 13:18:07 +0100

    4 completado

--- enunciado4.py
+++ enunciado4.py
@@ -6,51 +6,35 @@ from lxml import etree
 doc = etree.parse('batallasjuegotronos.xml')
 raiz=doc.getroot()
 
-#rey=raw_input("introduce el nombre de un rey")
+rey=raw_input("introduce el nombre de un rey: ")
 #ej. Robb Stark,Stannis Baratheon
-rey="Robb Stark"
+#rey="Robb Stark"
 
 #he eliminado el caracter 'ñ' del xml porque xpath no aceptaba ese caracter a pesar de que ambos ficheros estan codificados con utf-8
 
 rutarey="//rey_atacante/text()|//rey_defensor/text()"
-
-#ejercito="//ejercito_atacante[rey_atacante/text()='"+rey+"']/@ejercito"
-
 ejercito="//ejercito_atacante[rey_atacante/text()='"+rey+"']/@ejercito| //ejercito_defensor[rey_defensor/text()='"+rey+"']/@ejercito"
 
-
+#obtenemos la lista de reyes y si el nombre introducido se encuetra en él obtiene el tamaño de sus ejercitos en las diferentes batallas
 reyes=raiz.xpath(rutarey)
 if rey in reyes:
     ejercitos=raiz.xpath(ejercito)
-
-print ejercitos
-
-#pasamos la lista de strings a una lista de numeros pero al mapear la lista como integer da una excepcion ValueError debido que no puede
-#pasar a numero un valor como ' ' asi que usamos lo siguiente, la lista sin repetir y una estructura con la misma funcion que map(int,list)
-
-
-x = list(set(ejercitos))
-
-mayorejercito=0
-
-for elem in x:
-   if elem.strip():
-       n = int(elem)
-       if n>mayorejercito:
-           mayorejercito=n
-
-print mayorejercito
-
-#ruta ejercito contrincante con el mismo nodo padre:
-
-ejenemigo="//contendientes/ejercito_atacante[@ejercito="+str(mayorejercito)+"][//rey_atacante/text()='"+rey+"']/following-sibling::ejercito_defensor"
-enemigo=raiz.xpath(ejenemigo)
-
-ejercitoenemigo= enemigo[0].get('ejercito')
-
-print ejercitoenemigo
-
-
-#falta mostrar el nombre de dicha batalla
-
-#print "la mayor batalla de",rey,"fue",nombrebatalla,"en la que participaron",mayorejercito+ejercitoenemigo
+    #pasamos la lista de strings a una lista de numeros pero al mapear la lista como integer da una excepcion ValueError debido que no puede
+    #pasar a numero un valor como ' ' asi que usamos lo siguiente, la lista sin repetir y una estructura con la misma funcion que map(int,list)
+    #y obtenemos el mayor ejercito con el que ha participado en una batalla
+    x = list(set(ejercitos))
+    mayorejercito=0
+    for elem in x:
+       if elem.strip():
+           n = int(elem)
+           if n>mayorejercito:
+               mayorejercito=n
+    #ruta con el mismo nodo padre, obtenemos el tamaño del ejercito contrincante del mayor ejercito del rey introducido:
+    ejenemigo="//contendientes/ejercito_atacante[@ejercito="+str(mayorejercito)+"][//rey_atacante/text()='"+rey+"']/following-sibling::ejercito_defensor"
+    enemigo=raiz.xpath(ejenemigo)
+    ejercitoenemigo= int(enemigo[0].get('ejercito'))
+    rutanombre="//batalla/contendientes[((ejercito_atacante/@ejercito='15000' or ejercito_defensor/@ejercito='15000') and (ejercito_atacante/@ejercito='4000' or ejercito_defensor/@ejercito='4000')) and //rey_atacante/text()='Joffrey/Tommen Baratheon']/../nombre/text()"
+    nombrebatalla=raiz.xpath(rutanombre)
+    print "\nla mayor batalla de "+rey+" es "+nombrebatalla[0]+" en la que participaron "+str(mayorejercito+ejercitoenemigo)+" hombres"
+else:
+    print "nombre introducido no válido"
